1127bd76bea37fd25d974472be2c5dc792cc7c07
alex-hayhoe <33250234+alex-hayhoe@users.noreply.github.com>
2024-04-26 00:36:51 +0100

    single file to test deploy

--- modules/main.tf
+++ modules/main.tf
@@ -2,27 +2,146 @@ provider "kubernetes" {
   config_context_cluster   = "minikube"
 }
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+resource "kubernetes_namespace" "dev" {
+  metadata {
+    name = "var.namespace_name"
+  }
+}
+
+resource "kubernetes_deployment" "mysql" {
+  metadata {
+    namespace = var.namespace_name
+    name      = "mysql"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "mysql"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "mysql"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mysql"
+          image = "mysql:latest"
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = "var.mysql_root_password"
+          }
+
+          ports {
+            container_port = 3306
+          }
+        }
+      }
     }
   }
 }
 
-module "namespace" {
-  source = "./modules/namespace"
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    namespace = kubernetes_namespace.dev.metadata.0.name
+    name      = "nginx"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
 }
 
-module "mysql" {
-  source = "./modules/mysql"
-}
+resource "kubernetes_deployment" "minio" {
+  metadata {
+    namespace = kubernetes_namespace.dev.metadata.0.name
+    name      = "minio"
+  }
 
-module "nginx" {
-  source = "./modules/nginx"
-}
+  spec {
+    replicas = 1
 
-module "minio" {
-  source = "./modules/minio"
+    selector {
+      match_labels = {
+        app = "minio"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "minio"
+        }
+      }
+
+      spec {
+        container {
+          name  = "minio"
+          image = "minio/minio:latest"
+
+          env {
+            name  = "MINIO_ACCESS_KEY"
+            value = "var.minio_access_key"
+          }
+
+          env {
+            name  = "MINIO_SECRET_KEY"
+            value = "var.minio_secret_key"
+          }
+
+          ports {
+            container_port = 9000
+          }
+
+          args = [
+            "server",
+            "/data"
+          ]
+
+          volume_mount {
+            name       = "minio-storage"
+            mount_path = "/data"
+          }
+        }
+
+        volume {
+          name = "minio-storage"
+          empty_dir {}
+        }
+      }
+    }
+  }
 }
