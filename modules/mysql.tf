92eac8f36c08827a8c38d70b4d8a520c08f3b738
alex-hayhoe <33250234+alex-hayhoe@users.noreply.github.com>
2024-04-26 17:11:58 +0100

    add mysql init

--- modules/mysql.tf
+++ modules/mysql.tf
@@ -143,9 +143,55 @@ resource "kubernetes_job" "mysql-init" {
             value = var.mysql_db_name
           }
 
-          command = ["sh", "-c", "mysql -h mysql-service -P 3306 --protocol=tcp -uroot -p${var.mysql_root_password} -e 'CREATE DATABASE IF NOT EXISTS ${var.mysql_db_name}'"] && ["sh", "-c", "mysql -h mysql-service -P 3306 --protocol=tcp -uroot -p${var.mysql_root_password} -e 'CREATE USER '${var.mysql_username}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${var.mysql_username_password}'"]
+          command = ["sh", "-c", "mysql -h mysql-service -P 3306 --protocol=tcp -uroot -p${var.mysql_root_password} -e 'CREATE DATABASE IF NOT EXISTS ${var.mysql_db_name}'"]        
         }
+        restart_policy = "Never"
+      }
+    }
+  }
+}
 
+resource "kubernetes_job" "mysql-init-user" {
+  metadata {
+    name      = "mysql-init"
+    namespace = var.namespace_name
+  }
+
+  spec {
+    template {
+      metadata {
+        labels = {
+          app = "mysql-init"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mysql-init"
+          image = "mysql:latest"
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = var.mysql_root_password
+          }
+
+          env {
+            name  = "MYSQL_USER"
+            value = var.mysql_username
+          }          
+
+          env {
+            name  = "MYSQL_USER_PW"
+            value = var.mysql_username_password
+          }
+
+          env {
+            name  = "MYSQL_DATABASE"
+            value = var.mysql_db_name
+          }
+        
+          command = ["sh", "-c", "mysql -h mysql-service -P 3306 --protocol=tcp -uroot -p${var.mysql_root_password} -e 'CREATE USER '${var.mysql_username}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${var.mysql_username_password}'"]
+        }
         restart_policy = "Never"
       }
     }
