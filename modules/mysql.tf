f0b01119dcd2075b4c72c4d6acbd3a372e26231a
alex-hayhoe <33250234+alex-hayhoe@users.noreply.github.com>
2024-04-26 15:07:12 +0100

    add mysql init

--- modules/mysql.tf
+++ modules/mysql.tf
@@ -35,62 +35,59 @@ resource "kubernetes_service" "mysql" {
   }
 }
 
-resource "kubernetes_deployment" "mysql" {
+resource "kubernetes_pod" "mysql-init" {
   metadata {
+    name      = "mysql-init"
     namespace = var.namespace_name
-    name      = "mysql"
   }
 
   spec {
-    replicas = 1
+    init_container {
+      name  = "init-mysql"
+      image = "mysql:latest"
 
-    selector {
-      match_labels = {
-        app = "mysql"
+      env {
+        name  = "MYSQL_ROOT_PASSWORD"
+        value = var.mysql_root_password
+      }
+
+      env {
+        name  = "MYSQL_DATABASE"
+        value = var.mysql_db_name
+      }
+
+      volume_mount {
+        name       = "mysql-data"
+        mount_path = "/var/lib/mysql"
+      }
+
+      command = ["sh", "-c", "mysql -h localhost -uroot -p${var.mysql_root_password} -e 'CREATE DATABASE IF NOT EXISTS ${var.mysql_db_name}'"]
+    }
+
+    container {
+      name  = "mysql"
+      image = "mysql:latest"
+
+      env {
+        name  = "MYSQL_ROOT_PASSWORD"
+        value = var.mysql_root_password
+      }
+
+      ports {
+        container_port = 3306
+      }
+
+      volume_mount {
+        name       = "mysql-data"
+        mount_path = "/var/lib/mysql"
       }
     }
 
-    template {
-      metadata {
-        labels = {
-          app = "mysql"
-        }
-      }
-
-      spec {
-        container {
-          name  = "mysql"
-          image = "mysql:latest"
-
-          env {
-            name  = "MYSQL_ROOT_PASSWORD"
-            value = var.mysql_root_password
-          }
-          
-          env {
-            name  = "MYSQL_DATABASE"
-            value = var.mysql_db_name
-          }          
-
-          port {
-            container_port = 3306
-          }
-
-        
-          volume_mount {
-            name       = "mysql-data"
-            mount_path = "/var/lib/mysql"
-          }
-        }
-
-        volume {
-           name = "mysql-data"
-           persistent_volume_claim {
-           claim_name = kubernetes_persistent_volume_claim.mysql_data.metadata[0].name
-          }
-        }
+    volume {
+      name = "mysql-data"
+      persistent_volume_claim {
+        claim_name = kubernetes_persistent_volume_claim.mysql_data.metadata[0].name
       }
     }
   }
 }
-
