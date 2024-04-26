0c25199775a41956099f7e3f1cb1a92c770075a8
alex-hayhoe <33250234+alex-hayhoe@users.noreply.github.com>
2024-04-26 11:10:14 +0100

    helm chart nginx resource change

--- modules/nginx.tf
+++ modules/nginx.tf
@@ -1,39 +1,50 @@
-
 resource "kubernetes_deployment" "nginx" {
   metadata {
+    name = "scalable-nginx-example"
     namespace = var.namespace_name
-    name      = "nginx"
+    labels = {
+      App = "ScalableNginxExample"
+    }
   }
 
   spec {
-    replicas = 1
-
+    replicas = 2
     selector {
       match_labels = {
-        app = "nginx"
+        App = "ScalableNginxExample"
       }
     }
-
     template {
       metadata {
         labels = {
-          app = "nginx"
+          App = "ScalableNginxExample"
         }
       }
-
       spec {
         container {
-          name  = "nginx"
-          image = "nginx:latest"
+          image = "nginx:1.7.8"
+          name  = "example"
 
           port {
             container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
           }
         }
       }
     }
   }
 }
+
 
 resource "kubernetes_service" "nginx-svc" {
   metadata {
