9f81fad7f40f958f1c6678282ca7413a47a5190b
alex-hayhoe <33250234+alex-hayhoe@users.noreply.github.com>
2024-04-26 20:08:24 +0100

    add mysql init

--- modules/mysql.tf
+++ modules/mysql.tf
@@ -197,3 +197,50 @@ resource "kubernetes_job" "mysql-init-user" {
     }
   }
 }
+
+resource "kubernetes_job" "mysql-init-user-privikedges" {
+  metadata {
+    name      = "mysql-init-user-priviledges"
+    namespace = var.namespace_name
+  }
+
+  spec {
+    template {
+      metadata {
+        labels = {
+          app = "mysql-init-user-priviledges"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mysql-init-user-priviledges"
+          image = "mysql:latest"
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = var.mysql_root_password
+          }
+
+          env {
+            name  = "MYSQL_USER"
+            value = var.mysql_username
+          }          
+
+          env {
+            name  = "MYSQL_USER_PW"
+            value = var.mysql_username_password
+          }
+
+          env {
+            name  = "MYSQL_DATABASE"
+            value = var.mysql_db_name
+          }
+        
+          command = ["sh", "-c", "mysql -h mysql-service -P 3306 --protocol=tcp -uroot -p${var.mysql_root_password} -e GRANT ALL PRIVILEGES ON ${var.mysql_db_name} TO ${var.mysql_username}@'localhost'"]
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+}
